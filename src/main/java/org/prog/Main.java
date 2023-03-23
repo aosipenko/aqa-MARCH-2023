10ab80cace7e4e258f5829c2a371c7a3d4f4d83e
oo965787 <oleksii.osypenko@telenor.se>
2023-03-23 20:10:19 +0100

    Session 4: maven + selenium

--- src/main/java/org/prog/Main.java
+++ src/main/java/org/prog/Main.java
@@ -1,51 +1,14 @@
 package org.prog;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
 public class Main {
 
-    /*
-     **************************
-     ******varTwo**************
-     ********==****************
-     *********varOne***********
-     **************************
-     ********01****************
-     */
 
     public static void main(String[] args) {
-        AnotherClass varOne = new AnotherClass();
-        AnotherClass varTwo = new AnotherClass();
-        AnotherClass varThree = varOne;
-        AnotherClass varFour = varOne;
-//        varOne.aString = "varOneValue";
-//        varTwo.aString = "varTwoValue";
-
-
-//        System.out.println(varOne.aString);
-//        System.out.println(varTwo.aString);
-
-        varOne.aString = "a";
-        varTwo.aString = "a";
-
-        System.out.println(varOne == varTwo);
-        System.out.println(varOne == varThree);
-        System.out.println(varThree == varFour);
-
-//        boolean b = true;
-//        char c = 1;
-//        short s = 1;
-//        int i = 0;
-//        long l = 10;
-//        double d = 1.00D;
-//        float f = 5.24444F;
-
-        // String pool
-//        String myString = "a";
-//        myString = myString + "b";
-//        System.out.println(myString);
-//
-        Object o = new Object();
-        "a".equals("a");
-        o.equals(new Object());
-        varOne.equals(varTwo);
+        WebDriver webDriver = new ChromeDriver();
+        webDriver.get("https://google.com/");
+        webDriver.quit();
     }
 }
