70a806f02b343d7598d94ce12d44b124a2581ed2
oo965787 <oleksii.osypenko@telenor.se>
2023-03-28 18:42:09 +0200

    Session 5: selenium essentials

--- src/main/java/org/prog/Main.java
+++ src/main/java/org/prog/Main.java
@@ -1,14 +1,26 @@
 package org.prog;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
-public class Main {
+import java.util.List;
 
+public class Main {
 
     public static void main(String[] args) {
         WebDriver webDriver = new ChromeDriver();
         webDriver.get("https://google.com/");
+
+        List<WebElement> buttons = webDriver.findElements(By.tagName("button"));
+        if (buttons.size() == 5) {
+            WebElement acceptCookiesButton = buttons.get(3);
+            acceptCookiesButton.click();
+        } else {
+            System.out.println("No cookies form is displayed!");
+        }
+
         webDriver.quit();
     }
 }
